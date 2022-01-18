c3f1c2524733f84ed2317a856fa8819b95d45da0
Alin Manole <alin.manole@yahoo.com>
2022-01-18 10:35:42 +0100

    update

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ const session = require("express-session")
 const passport = require("./config/passport")
 const mongoose = require("mongoose")
 
+app.use(express.json())
+app.use(express.static('public'))
 
 //routes
 const usersRoutes = require("./routes/users")
@@ -13,14 +15,25 @@ const tweetsRoutes = require("./routes/tweets")
 const commentsRoutes = require("./routes/comments")
 const authRoutes = require("./routes/auth")
 const uploadRoutes = require("./routes/upProfilePicture.js")
+const followRoutes = require('./routes/follow')
+
+//Mongo Start
+const dbName = 'TwitterDB'
+  const dbUrl = 'mongodb+srv://admin:admin@twitterclone.nzeai.mongodb.net/TwitterDB'
+
+  mongoose.connect(dbUrl)
+const db = mongoose.connection
+
+db.on('error', (err) => {
+  console.log(err)
+})
+
+db.once('open', () => {
+  console.log(`Connection to ${dbName} established`)
+})
+// Mongo Finished
 
 
-
-require('./models/Tweet')
-require('./models/User')
-require('./models/Comment')
-
-app.use(express.json())
 
 app.use(session({
   secret: "secret",
@@ -36,13 +49,13 @@ app.use(cors({
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.use(express.json())
-app.use(express.static("public"))
+
+app.use('/', authRoutes)
 app.use('/users', usersRoutes)
 app.use('/tweets', tweetsRoutes)
-app.use('/comments', commentsRoutes)
-app.use('/auth', authRoutes)
 app.use('/upload', uploadRoutes)
+app.use('/comments', commentsRoutes)
+app.use('/follow', followRoutes)
 
 
 app.listen(port, () => {
--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -1,12 +1,12 @@
 const { Schema, model } = require("mongoose")
 
 const CommentSchema = Schema({
-  content :  { type : String },
+  content :  { type : String, maxLength: 280,
+    required: true },
   author : { type : Schema.Types.ObjectId, ref:"User" },
 
   tweet : { type: Schema.Types.ObjectId, ref: "Tweet" },
-
-  
+ 
 },{
   timestamps: true,
 })
